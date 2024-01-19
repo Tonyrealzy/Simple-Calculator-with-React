1b529da6af70e991a7fd205ccb66cd3c3ee7adcd
Tonyrealzy <umehobiarinze2@gmail.com>
2024-01-19 09:37:16 +0100

    Push It!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 
 function App() {
+
+  const [display, setDisplay] = React.useState(0);
+  function handleNumber(value){
+    alert('handle number click: ' + value);
+    setDisplay(value);
+  }
+  
+  function handleOperator(){}
+
+  function CalcButton(props){
+    return <button className={props.className} onClick={props.onClick(props.value)}>{props.value}</button>
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-        
-      </header>
+    <div className="calculator">
+
+      <div className="display">{display}</div>
+
+      <CalcButton value="7" onClick={handleNumber}/>
+      <CalcButton value="8" onClick={handleNumber}/>
+      <CalcButton value="9" onClick={handleNumber}/>
+      <CalcButton className="operator" onClick={handleOperator} value="/" />
+
+      <CalcButton value="4" onClick={handleNumber}/>
+      <CalcButton value="5" onClick={handleNumber}/>
+      <CalcButton value="6" onClick={handleNumber}/>
+      <CalcButton className="operator" onClick={handleOperator} value="*" />
+
+      <CalcButton value="1" onClick={handleNumber}/>
+      <CalcButton value="2" onClick={handleNumber}/>
+      <CalcButton value="3" onClick={handleNumber}/>
+      <CalcButton className="operator" onClick={handleOperator} value="-" />
+
+      <CalcButton value="C" />
+      <CalcButton value="0" onClick={handleNumber}/>
+      <CalcButton value="=" onClick={handleOperator}/>
+      <CalcButton className="operator" onClick={handleOperator} value="+" />
     </div>
   );
 }
